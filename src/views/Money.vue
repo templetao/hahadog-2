778ec7b77dba2c49a8da73770c4c4de7170ccdd2
taotao <1985504469@qq.com>
2023-03-13 18:43:10 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
     <Nav/>
   </div>
 
@@ -13,5 +16,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>

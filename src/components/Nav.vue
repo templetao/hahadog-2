512c9a454d446279398520e31ee4a60080703dc2
taotao <1985504469@qq.com>
2023-03-13 21:46:24 +0800

    点中 tab 变色

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav class="nav">
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
 
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -38,11 +38,14 @@ nav {
     align-items: center;
     flex-direction: column;
 
-
     .icon {
       width: 32px;
       height: 32px;
     }
+  }
+
+  > .item.selected {
+    color: yellowgreen;
   }
 }
 </style>

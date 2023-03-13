36f8765a5894ea065e1eb1e3039e4ed3aaa04449
taotao <1985504469@qq.com>
2023-03-13 17:58:28 +0800

    创建æ三个页面

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+
+module.exports = {
   lintOnSave: false
-})
+}

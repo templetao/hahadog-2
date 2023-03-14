1b6f6cc6ec50397c3fcb8e539b550fa05a4fd601
taotao <1985504469@qq.com>
2023-03-14 18:17:36 +0800

    用 ts 替换 js

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -11,27 +11,42 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Types',
-  props: ['xxx'],
-  data() {
-    return {
-      type: '-' // '-'表示支出, '+'表示收入
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue {
+  type = '-'; // '-'表示支出, '+'表示收入
+
+  selecType(type: string) { // type 只能是‘-’ 和 ‘+’中的一个
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is not know');
     }
-  },
-  mounted() {
-    console.log(this.xxx)
-  },
-  methods: {
-    selecType(type) { // type 只能是‘-’ 和 ‘+’中的一个
-      if (type !== '-' && type !== '+') {
-        throw new Error('type is not know')
-      }
-      this.type = type
-    }
+    this.type = type;
   }
+
 }
+// export default {
+//   name: 'Types',
+//   props: ['xxx'],
+//   data() {
+//     return {
+//       type: '-' // '-'表示支出, '+'表示收入
+//     }
+//   },
+//   mounted() {
+//     console.log(this.xxx)
+//   },
+//   methods: {
+//     selecType(type) { // type 只能是‘-’ 和 ‘+’中的一个
+//       if (type !== '-' && type !== '+') {
+//         throw new Error('type is not know')
+//       }
+//       this.type = type
+//     }
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>

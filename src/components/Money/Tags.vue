69b2947e4027ca18c9410719ec425a50dd3aab90
taotao <1985504469@qq.com>
2023-03-15 22:20:30 +0800

    完成 新增标签 模块 Money组件 .sync

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag"
@@ -19,15 +19,24 @@ import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Tags extends Vue {
-  @Prop(Array) dataSource: string[] | undefined;
+  @Prop(Array) readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
-      this.selectedTags.splice(index, 1)
+      this.selectedTags.splice(index, 1);
     } else {
-      this.selectedTags.push(tag)
+      this.selectedTags.push(tag);
+    }
+  }
+
+  create() {
+    const name = window.prompt('请输入标签名');
+    if (name === '') {
+      window.alert('标签名不能为空')
+    } else if (this.dataSource){
+      this.$emit('update:dataSource',[...this.dataSource, name])
     }
   }
 }

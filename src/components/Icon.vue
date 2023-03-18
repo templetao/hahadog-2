8e69e071133d9a0bbb1e934f11bf6b01f5c8183a
taotao <1985504469@qq.com>
2023-03-18 21:53:06 +0800

    完成 id生成器 并修复几个 bug

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -15,11 +15,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .icon{
-    width: 1em;
-    height: 1em;
-    vertical-align: -0.15em;
-    fill: currentColor;
-    overflow: hidden;
-  }
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagList';
 type Tag = {
   id: string;
@@ -23,7 +25,8 @@ const tagListModel: TagListModel = {
   create(name) {
     const names = this.data.map(item => item.name);
     if (names.indexOf(name) >= 0) { throw 'duplicated'; }
-    this.data.push({id: name, name: name});
+    const id = createId().toString();
+    this.data.push({id, name});
     this.save();
     return 'success';
   },

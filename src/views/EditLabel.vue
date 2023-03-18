60d39c5ec226f83a3e20562ef220738e53eb8324
taotao <1985504469@qq.com>
2023-03-18 21:23:58 +0800

    完成了 Edit tag 的 update 和 remove 功能

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,10 +6,12 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name"
+                @update:value="update"
+                field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -25,7 +27,7 @@ import Button from '@/components/Button.vue';
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
-  tag?: {id:string, name: string} = undefined;
+  tag?: { id: string, name: string } = undefined;
 
   created() {
     const id = this.$route.params.id;
@@ -36,6 +38,18 @@ export default class EditLabel extends Vue {
       this.tag = tag;
     } else {
       this.$router.replace('/404');
+    }
+  }
+
+  update(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+
+  remove() {
+    if (this.tag) {
+      tagListModel.remove(this.tag.id);
     }
   }
 }

cd03f1184add194fad49042f9812a080f72a3108
taotao <1985504469@qq.com>
2023-03-20 17:04:02 +0800

    解决 Money 部分 标签异常问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -8,7 +8,7 @@
                 @update:value="onUpdateNotes"/>
     </div>
 
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    <Tags />
   </Layout>
 
 </template>
@@ -27,15 +27,10 @@ import store from '@/store/index2';
 })
 
 export default class Money extends Vue {
-  tags = store.tagList;
   recordList = store.recordList;
   record: { amount: number; notes: string; type: string; tags: string[] } = {
     tags: [], notes: '', type: '-', amount: 0,
   };
-
-  onUpdateTags(value: string[]) {
-    this.record.tags = value;
-  }
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
